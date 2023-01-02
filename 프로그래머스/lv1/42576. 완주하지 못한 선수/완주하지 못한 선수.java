71a404048ba67603ad559b68ced2392c2a1a1458
ifksj05 <118642852+ifksj05@users.noreply.github.com>
2023-01-02 15:41:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.45 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,22 +1,15 @@
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        HashMap<String, Integer> map = new HashMap<>();
-        for (String player : participant) 
-            map.put(player, map.getOrDefault(player, 0) + 1);
-        for (String player : completion) 
-            map.put(player, map.get(player) - 1);
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
 
-            Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
-
-        while(iter.hasNext()){
-            Map.Entry<String, Integer> entry = iter.next();
-            if (entry.getValue() != 0){
-                answer = entry.getKey();
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
                 break;
             }
         }
